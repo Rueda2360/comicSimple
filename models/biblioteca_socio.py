1251369d46cac7c9b3245b40d2e4911f25e0e250
Rueda <alerueda2360@gmail.com>
2024-02-17 18:58:48 +0100

    rec_name en socio

--- models/biblioteca_socio.py
+++ models/biblioteca_socio.py
@@ -6,9 +6,17 @@ from odoo import models, fields, api
 class BibliotecaSocio(models.Model):
     _name = 'biblioteca.socio'
     _description = 'Socio de la biblioteca'
-
+    _rec_name = 'nombreCompleto'
+    
     nombre = fields.Char('Nombre')
     apellido = fields.Char('Apellido')
     identificador = fields.Char('Identificador')
 
     #idSocio = fields.Many2many('biblioteca.prestamo', 'idSocio', string='Préstamos')
+
+    nombreCompleto = fields.Char('Nombre Completo', compute='_compute_nombre_completo', store=True)
+
+    @api.depends('nombre', 'apellido')
+    def _compute_nombre_completo(self):
+        for record in self:
+            record.nombreCompleto = f"{record.nombre} {record.apellido}"
